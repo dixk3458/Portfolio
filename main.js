4c69ba197bacc2fe4d96f7ef7b7ca7efca6b6b1c
dixk3458 <dixk3458@naver.com>
2022-08-01 18:50:10 +0900

    JS : Home contact

--- main.js
+++ main.js
@@ -14,13 +14,24 @@ document.addEventListener('scroll', () => {
 // Handle scrolling when tapping  on the navbar menu
 
 const navbarMenu = document.querySelector('.navbar__menu');
-document.addEventListener('click', event => {
+navbarMenu.addEventListener('click', event => {
   const target = event.target;
   const link = target.dataset.link;
-  if (link == null) {
+  if (link === null) {
     return;
   } else {
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior: 'smooth' });
+    scrollIntoView(link);
   }
 });
+
+// Handle click on "contact me" button on home
+
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
