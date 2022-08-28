7db9bde6a55c7d94cd712e5bbd9ab7f63305a085
dixk3458 <dixk3458@naver.com>
2022-08-28 21:33:37 +0900

    JS : Navbar toggle and Project UI tw

--- main.js
+++ main.js
@@ -19,7 +19,14 @@ navbarMenu.addEventListener('click', event => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
 // Handle click on "Contact Me" button on home
@@ -61,6 +68,14 @@ workBtnContainer.addEventListener('click', e => {
   if (filter === undefined) {
     return;
   }
+
+  // Remove selection from the previous item and select the new one
+  const active = document.querySelector('.category__btn.selected');
+  active.classList.remove('selected');
+  const target =
+    e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+  target.classList.add('selected');
+
   projectContainer.classList.add('anim-out');
   setTimeout(() => {
     projects.forEach(project => {
